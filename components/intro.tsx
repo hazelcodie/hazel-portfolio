7e58e37c01fd9bcb97e46091411e3684f42caf69
hazelcodie <hazelmontiaora@gmail.com>
2025-03-29 21:21:17 +0800

    portfolio using nextjs

---
+++ components/intro.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+const Intro = () => {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full p-8 rounded-lg shadow-lg sm:p-20">
+        <div className="flex items-center justify-center w-full h-full">
+            
+      <h1 className="text-4xl font-bold">Hi, I'm Hazel </h1>
+      <p className="mt-4 text-lg">
+        A motivated UI/UX Designer and Front-End Developer passionate about
+        creating intuitive and visually appealing digital experiences. Currently
+        pursuing and continuously learning new technologies, frameworks, and
+        design trends to enhance user interactions.
+      </p>
+    </div>
+    </div>
+  );
+};
+
+export default Intro;
