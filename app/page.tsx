e7a299f66867e0ad5ac40ce4207a285c1128749b
hazelcodie <hazelmontiaora@gmail.com>
2025-04-11 13:45:47 +0800

    hero-final

--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,17 @@ import AboutMe from "@/components/about-me";
 import { TerminalDemo } from "@/components/skills";
 import {DesignProject} from "@/components/designProject";
 import "./globals.css";
-import { Dock, DockIcon } from "@/components/magicui/dock";
-
 
 export default function Home() {
   return (
     <div className="snap-y snap-mandatory min-h-screen overflow-y-scroll  overflow-x-hidden bg-fuchsia-100">
-        <Dock>
-          <DockIcon>
+        
         <Navbar />
         <Intro />
         <AboutMe />
         <TerminalDemo />
         <DesignProject/>
-          </DockIcon>
-        </Dock>
+          
     </div>
   );
 }
--- components/intro.tsx
+++ components/intro.tsx
@@ -8,20 +8,19 @@ const Intro = () => {
     <div className="w-full pt-[4vh] md:pt-[12vh] bg-fuchsia-100 overflow-hidden">
       <div className="flex justify-center flex-col w-4/5 h-full mx-auto">
         <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-12 h-full">
-          
           <div className="flex flex-col justify-center gap-4">
             <h1 className="text-2xl md:text-3xl lg:text-4xl text-black font-bold">
               I'm Hazel and I specialized in
             </h1>
 
-            <div className="flex flex-col gap-3">
-              <span className="bg-[#c7e6f7] text-black px-4 py-2 rounded-lg text-lg w-fit">
+            <div className="flex flex-col items-start gap-3">
+              <span className="bg-[#A0CAE0] text-white px-4 py-2 rounded-lg text-[22px] font-medium leading-snug inline-block">
                 UI and UX Design
               </span>
-              <span className="bg-[#f8d4e4] text-black px-4 py-2 rounded-lg text-lg w-fit">
+              <span className="bg-[#E8AAD1] text-white px-8 py-2 rounded-lg text-[22px] font-medium inline-block ml-8">
                 Front-end Development
               </span>
-              <span className="bg-[#2d49c2] text-white px-4 py-2 rounded-lg text-lg w-fit">
+              <span className="bg-[#2d49c2] text-white px-6 py-2 rounded-lg text-[22px] font-medium inline-block ml-3">
                 Web Development
               </span>
             </div>
@@ -31,7 +30,6 @@ const Intro = () => {
             </button>
           </div>
 
-         
           <div className="flex items-center justify-center lg:justify-end h-full">
             <Image
               src={profile}
